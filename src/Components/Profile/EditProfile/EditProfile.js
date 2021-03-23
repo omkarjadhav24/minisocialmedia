2eb25af49cd3412b1bf0647fd1a353ee9defa501
omkar Jadhav <omkarj@wemotiveforge.com>
2021-03-23 16:39:01 +0530

    E. profile Edit  then redirect to profile page

--- src/Components/Profile/EditProfile/EditProfile.js
+++ src/Components/Profile/EditProfile/EditProfile.js
@@ -6,26 +6,29 @@ import background from '../../../assets/log3.jpg'
 import {connect} from 'react-redux'
 import {editProfile} from '../../../Actions/EditProfile'
 import axios from 'axios';
+import moment from 'moment'
+import {Redirect } from 'react-router-dom';
+
 class EditProfile extends Component{
   
     componentDidMount(){
         console.log(this.props);
     }
     state={
-        name:'',
+        name:this.props.name,
         nameError:'',
-        age:'',
+        age:this.props.age,
         ageError:'',
-        date:'',
-        date:'',
+        date:this.props.dob,
         dateError:'',
-        gender:'',
+        gender:this.props.gender,
         genderError:'',
-        email:'',
+        email:this.props.email,
         emailError:'',
         password:'',
         passworError:'',
-        login:false
+        login:false,
+        redirect:false
     }
     checkValidity(){
         if( !isNaN(this.state.name))
@@ -69,7 +72,7 @@ class EditProfile extends Component{
         }
        
     }
-
+   
     submitHandler = (event) => {
         event.preventDefault();
         const editData={
@@ -89,6 +92,7 @@ class EditProfile extends Component{
           })
         .then(res=>{
             console.log(res)
+            this.setState({redirect:true})
         })
         .catch(err=>{
             console.log(err);
@@ -103,38 +107,40 @@ class EditProfile extends Component{
     }
     
     render(){
-       
+        if(this.state.redirect){
+            return <Redirect to="/profile" />
+        }
         return(
             <div className={classes.SignUp} style={{ backgroundImage: `url(${background})` }} >
                 <form onSubmit={this.submitHandler}>
                     <div className="form-group" >
                     <label className="font-weight-bold" >Name</label>
-                    <input className="form-control-sm" type="text" value={this.props.name ?    this.props.name :this.state.name}  onChange={( event ) => this.setState( { name: event.target.value } )} className="form-control"  placeholder="Enter your Name"/>
+                    <input className="form-control-sm" type="text" value={this.state.name}  onChange={( event ) => this.setState( { name: event.target.value } )} className="form-control"  placeholder="Enter your Name"/>
                     <span className=" font-weight-bold text-danger font-italic">{this.state.nameError}</span>
                     </div>
                     <div className="form-group" >
                     <label className="font-weight-bold" >Age</label>
-                    <input type="text" value={this.props.age ?    this.props.age :this.state.age}  onChange={( event ) => this.setState( { age: event.target.value } )} className="form-control form-control-sm"   placeholder="Enter your Age"/>
+                    <input type="text" value={this.state.age}  onChange={( event ) => this.setState( { age: event.target.value } )} className="form-control form-control-sm"   placeholder="Enter your Age"/>
                     <span className="font-weight-bold text-danger font-italic">{this.state.ageError}</span>
                     </div>
                     <div className="form-group ">
                     <label className="font-weight-bold">Date of Birth</label>
-                    <input className="form-control" value={this.props.dob ?    this.props.dob :null}  onChange={( event ) => this.setState( { date: event.target.value } )} type="date"  />
+                    <input className="form-control" value={moment.utc(this.state.date).format('DD/MM/YYYY')}  onChange={( event ) => this.setState( { date: event.target.value } )} type="date"  />
                     <span className=" font-weight-bold text-danger font-italic" >{this.state.dateError}</span>
                     </div>
                     <label className="font-weight-bold">Gender</label><br/>
                     <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="radio" value="Male" name="inlineRadioOptions"    onChange={( event ) => this.setState( { gender: event.target.value } )}/>
+                    <input className="form-check-input" type="radio"   value="Male" name="inlineRadioOptions"    onChange={( event ) => this.setState( { gender: event.target.value } )}/>
                     <label className="form-check-label" >Male</label>
                     </div>
                     <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="radio" value="FeMale" name="inlineRadioOptions"    onChange={( event ) => this.setState( { gender: event.target.value } )}/>
+                    <input className="form-check-input" type="radio"   value="FeMale" name="inlineRadioOptions"    onChange={( event ) => this.setState( { gender: event.target.value } )}/>
                     <label className="form-check-label" >FeMale</label><br/><br/>
                     </div>
                     <p className="font-weight-bold text-danger font-italic"> {this.state.genderError}</p>
                     <div className="form-group" >
                     <label  className="font-weight-bold" >Email address</label>
-                    <input type="email" value={this.state.email}  onChange={( event ) => this.setState( { email: event.target.value } )} className="form-control"   placeholder="Enter email"/>
+                    <input type="email" value={this.state.email} onChange={( event ) => this.setState( { email: event.target.value } )} className="form-control"   placeholder="Enter email"/>
                     <span className="font-weight-bold text-danger font-italic"> {this.state.emailError}</span>
                     </div>
                     <div className="form-group">
