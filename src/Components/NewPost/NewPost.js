627a4e53aed8eda045ccc56c99cb83ef31fed978
Omkar Jadhav <omkarj@wemotiveforge.com>
2021-04-05 15:02:21 +0530

    new-convert in functional comp. and add useState

--- src/Components/NewPost/NewPost.js
+++ src/Components/NewPost/NewPost.js
@@ -1,37 +1,27 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, {  useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import classes from './NewPost.module.css';
 import log2 from '../../assets/log2.jpg'
 import {connect} from 'react-redux'
 import {addPost} from '../../Actions/AddPost'
-class NewPost extends Component {
-    state = {
-        image:'',
-        description: '',
-        submitted: false,
-        titleError:'',
-        contentError:''
-        
-    }
-    imageFileHandler=(e)=>
+const NewPost =(props)=> {
+    const [image,setImage]=useState('')
+    const [description,setDescription]=useState('')
+    const [descriptionError,setDescriptionError]=useState('')
+    const [imageError,setImageError]=useState(false);
+   const imageFileHandler=(e)=>
     {
-        this.setState({image:e.target.files})
+        setImage(e.target.files[0].name)
     }
-    checkValidity(){ 
-        // if(!(this.state.image!=""))
+    const checkValidity=()=>{ 
+        // if(!(image!=""))
         // {
-        //     this.setState({
-        //         imageError:'Please Select Image'
-        //     });
+        //    setImageError('Please Select Image')
         // }
         // else 
-        if(!(this.state.description.length>=5))
+        if(!(description.length>=5))
         {
-            this.setState({
-                descriptionError:'Content is more than 5 char and less than 20'
-
-            })
+            setDescriptionError('Content is more than 5 char and less than 20')
         }
         else{
             return true;
@@ -39,19 +29,14 @@ class NewPost extends Component {
        
     }
    
-    componentDidMount () {
-        console.log( this.props );
-    }
-
-    postDataHandler = (event) => {
+   const  postDataHandler = (event) => {
         event.preventDefault();
-        console.log(this.state.image)
 
         const data = {
-            image: this.state.image,
-            description: this.state.description
+            image: image,
+            description: description
         };
-        if(this.checkValidity())
+        if(checkValidity())
         {
             console.log(data)
 
@@ -60,36 +45,34 @@ class NewPost extends Component {
             imgData.append("inputname", imagedata);
             let poData=imgData.get('inputname')
 
-            this.props.addPost(this.state.image,this.state.description)
+            props.addPost(image,description)
         }
        
     }
-    render () {
         // let redirect = null;
-        // if (this.state.submitted) {
+        // if (submitted) {
         //     redirect = <Redirect to="/posts" />;
         // }
        
         return (
             <div className={classes.NewPost} style={{ backgroundImage: `url(${log2})` }} >
                 {/* {redirect} */}
-                <form encType="multipart/form-data" action="">
+                {/* <form encType="multipart/form-data" action=""> */}
                 <h1 className="text-light" >Add a Post</h1>
                 <div className="form-group">
                 <label className="text-light">Image</label>
-                <input className="form-control" type="file"  onChange={this.imageFileHandler} />
-                <span className="font-weight-bold text-danger font-italic">{this.state.imageError}</span>
+                <input className="form-control" type="file"  onChange={()=>imageFileHandler()} />
+                <span className="font-weight-bold text-danger font-italic">{imageError}</span>
                 </div>
                 <div className="form-group">
                 <label className="text-light" >Description</label>
-                <textarea className="form-control" rows="2" value={this.state.description} onChange={( event ) => this.setState( { description: event.target.value } )} />
-                <span className="font-weight-bold text-danger font-italic">{this.state.descriptionError}</span><br/>
-                <button  className="btn btn-outline-warning mt-0" onClick={this.postDataHandler}>Add Post</button>
+                <textarea className="form-control" rows="2" value={description} onChange={( event ) => setDescription(event.target.value)} />
+                <span className="font-weight-bold text-danger font-italic">{descriptionError}</span><br/>
+                <button  className="btn btn-outline-warning mt-0" onClick={(event)=>postDataHandler(event)}>Add Post</button>
                 </div>
-                </form>
+                {/* </form> */}
             </div>
         );
-    }
 }
 
    const mapDispatchtoProps=(dispatch)=>{
