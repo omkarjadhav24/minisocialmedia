46d57c72b68d95a90a9244d53e416e3807924e9a
Omkar Jadhav <omkarj@wemotiveforge.com>
2021-04-06 16:29:29 +0530

    fix-restruct disptch and cmmts added

--- src/Actions/ShowProfile.js
+++ src/Actions/ShowProfile.js
@@ -2,9 +2,7 @@ import * as actionTypes from '../Actions/ActionType';
 import axios from 'axios';
 // code for when profile starts loading
 export const showProfileStart = () => {
-    return {
-        type: actionTypes.SHOW_PROFILE_START
-    };
+    return {type: actionTypes.SHOW_PROFILE_START};
 };
 // code for when api loads susscessfully data and storing the data
 export const showProfileSuccess = (name,gender,dob,profileId,email,age) => {
@@ -18,6 +16,7 @@ export const showProfileSuccess = (name,gender,dob,profileId,email,age) => {
         age:age
     };
 };
+// if api get failed
 export const showProfileFail = (error) => {
     return {
         type: actionTypes.SHOW_PROFILE_FAIL,
@@ -28,7 +27,7 @@ export const showProfile = () => {
     
    let token = localStorage.getItem('token')
     return dispatch => {
-        dispatch(showProfileStart());
+        dispatch(showProfileStart());// show profile api start
        console.log(token)
         axios.get('http://885039200eb0.ngrok.io/user/me',{
             
@@ -36,15 +35,7 @@ export const showProfile = () => {
               'Authorization': `Bearer ${token}` 
             }
           })
-        .then(res=>{
-            console.log(res)
-        dispatch(showProfileSuccess(res.data.name,res.data.gender,res.data.dob,res.data._id,res.data.email,res.data.age));
-
-        })
-        .catch(err=>{
-            console.log(err);
-        dispatch(showProfileFail(err.response.data.error));
-
-        })
+        .then(res=>dispatch(showProfileSuccess(res.data.name,res.data.gender,res.data.dob,res.data._id,res.data.email,res.data.age)))// if succses api
+        .catch(err=>dispatch(showProfileFail(err.response.data.error)))// if api get failed
     };
 };
