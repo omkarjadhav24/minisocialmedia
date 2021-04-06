5239f043e5a081b4b251aec1b1f6b2a5e1f53d5c
Omkar Jadhav <omkarj@wemotiveforge.com>
2021-04-06 16:27:11 +0530

    fix-cmmts added and restruct dispatch

--- src/Actions/Registration.js
+++ src/Actions/Registration.js
@@ -1,10 +1,10 @@
 import * as actionTypes from '../Actions/ActionType';
 import axios from 'axios';
+// registration start
 export const registrationStart = () => {
-    return {
-        type: actionTypes.REGISTRATION_START
-    };
+    return {type: actionTypes.REGISTRATION_START};
 };
+// if registration success
 export const registrationSuccess = (name,dob,gender,email,password,age,token) => {
     return {
         type: actionTypes.REGISTRATION_SUCCESS,
@@ -15,9 +15,9 @@ export const registrationSuccess = (name,dob,gender,email,password,age,token) =>
         gender:gender,
         token:token,
         age:age
-
     };
 };
+// if registration failed
 export const registrationFail = (error) => {
     return {
         type: actionTypes.REGISTRATION_FAIL,
@@ -32,22 +32,19 @@ export const registration = (name,dob,gender,email, password,age) => {
         email:email,
         password:password,
         age:age
-    }
+    }// params send through api
     let url='http://885039200eb0.ngrok.io/user';
     return dispatch => {
-        dispatch(registrationStart());
+        dispatch(registrationStart());// registration start
         console.log(authData)
         axios.post(url,authData)
         .then(res=>{
             console.log(res.data.token);
             console.log(res);
-            localStorage.setItem('token',res.data.token ) 
+            localStorage.setItem('token',res.data.token ) // set token
+            // if api get success
             dispatch(registrationSuccess(res.data.name,res.data.dob,res.data.gender,res.data.email,res.data.password,res.data.age,res.data.token));
-        }).catch(err=>{
-            console.log(err);
-            dispatch(registrationFail(err.response.data.error));
-
-        })
+        }).catch(err=>dispatch(registrationFail(err.response.data.error)))// if api failed
 
     };
 };
