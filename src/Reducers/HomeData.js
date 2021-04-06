dc0b75f81cff66e543e791fab015feffa0aa510d
Omkar Jadhav <omkarj@wemotiveforge.com>
2021-04-06 15:57:51 +0530

    fix- switch case and added cmmts

--- src/Reducers/HomeData.js
+++ src/Reducers/HomeData.js
@@ -7,13 +7,12 @@ const initialState = {
     error: null,
     home:[]
 };
-
 const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
         // updates when home starting
         case actionTypes.HOME_START: return {...state,loading:true}
         // state updates when api succesfully fetch data and storing the fetched data in home array
-        case actionTypes.HOME_SUCCESS: return{...state,home:action.homedata,homeDataid:action.homeDataid}
+        case actionTypes.HOME_SUCCESS: return{...state,home:action.homeData,homeDataid:action.homeDataid}
         //when api get failed then update state error and store error message
         case actionTypes.REGISTRATION_FAIL: return{...state,error:action.error}
         default:return state;
