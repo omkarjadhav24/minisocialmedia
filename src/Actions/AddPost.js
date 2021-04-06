013ecb35b22ec6653a1c752d4ff2bde6aba5f8a3
Omkar Jadhav <omkarj@wemotiveforge.com>
2021-04-06 16:15:59 +0530

    fix- cmmts added

--- src/Actions/AddPost.js
+++ src/Actions/AddPost.js
@@ -1,10 +1,10 @@
 import * as actionTypes from '../Actions/ActionType';
 import axios from 'axios';
+// add post start
 export const addPostStart = () => {
-    return {
-        type: actionTypes.ADD_POST_START
-    };
+    return {type: actionTypes.ADD_POST_START}
 };
+// add post api gets success then 
 export const addPostSuccess = (uploadImage,description) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
@@ -12,21 +12,23 @@ export const addPostSuccess = (uploadImage,description) => {
         description: description
     };
 };
+// if add post api gets failed then
 export const addPostFail = (error) => {
     return {
         type: actionTypes.AUTH_FAIL,
         error: error
     };
 };
+// for add post api
 export const addPost = (uploadImage , description) => {
     const postData={
         uploadImage :uploadImage ,
         description:description
-    }
+    }// param send with api
     let url='http://885039200eb0.ngrok.io/story/image';
     return dispatch => {
         let token=localStorage.getItem('token') 
-        dispatch(addPostStart());
+        dispatch(addPostStart());// when add post start
         console.log(token)
         console.log(postData);
         axios.post(url,postData, {
@@ -36,14 +38,8 @@ export const addPost = (uploadImage , description) => {
               "type": "formData"
             }
           })
-        .then(res=>{
-            console.log(res);
-            dispatch(addPostSuccess(res.data.image,res.data.description));
-        }).catch(err=>{
-            console.log(err)
-            dispatch(addPostFail(err.response.data.error));
-
-        })
+        .then(res=>dispatch(addPostSuccess(res.data.image,res.data.description)))// gets  res then execute
+        .catch(err=>dispatch(addPostFail(err.response.data.error)))// if api get failed
 
     };
 };
