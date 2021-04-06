09be01b182dc3d9d0097140be20af5552b8eba10
Omkar Jadhav <omkarj@wemotiveforge.com>
2021-04-06 14:59:54 +0530

    new-cmt added

--- src/Components/Requests/Request/Request.js
+++ src/Components/Requests/Request/Request.js
@@ -1,30 +1,20 @@
 import React, { useState } from 'react'
 import './Request.css'
 const Request =(props)=>{
-   
-    const [addfriend,setAddfriend]=useState(false);
-   
-    // friendRequestHandler=()=>{
-    //     let prevState=null;
-    //     prevState=this.state.addfriend
-    //     this.setState({
-    //         addfriend:!prevState
-    //     })
-    // }
-   
         return(
 <div className="mt-2 box bg-white">
         <div >
             {/* <img className="rounded-circle image" src="https://i.imgur.com/RpzrMR2.jpg" width="40"/> */}
                <p id="name">{props.name}</p>
+               {/* for accepting friend request */}
                <button  type="button" class="btn btn-primary">
                     <div onClick={props.accept} >
                     <i class="fa fa-user-plus" aria-hidden="true"></i>
                     <span>Accept</span> 
                     </div>
                 </button>
+                {/* for rejecting friend request */}
                <button  type="button" class="btn btn-primary">
-                
                     <div onClick={props.reject} >
                     <i class="fa fa-user-times" aria-hidden="true"></i>
                     <span>Reject</span>
